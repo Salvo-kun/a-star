11fe84013cb5eb0e3abe04e6ec800c62c66f5ec2
Salvo-kun <salvatore.licata99@gmail.com>
2022-07-11 12:35:30 +0200

    AStar | Implemented heap unit testing, still missing smart way to handle updates and finding inside heap.

--- heap/heap.c
+++ heap/heap.c
@@ -1,5 +1,8 @@
+#include <math.h>
 #include "heapPrivate.h"
 #include "../utils/util.h"
+
+// Public Methods
 
 /*
   Creates and return an heap, of a given type (0 if MIN_HEAP, otherwise MAX_HEAP)
@@ -10,17 +13,73 @@ heap_t *heap_create(int type)
 
   // Init the heap
   heap = (heap_t *)util_malloc(sizeof(heap_t));
-  heap->nodes = (node_t **)util_malloc(sizeof(node_t *));
+  heap->nodes = (node_t *)util_malloc(sizeof(node_t));
   heap->count = 0;
   heap->capacity = 1;
   heap->type = type == 0 ? MIN_HEAP : MAX_HEAP;
+
+  fprintf(stdout, "Created heap.\n");
+  heap_stats(heap);
 
   return heap;
 }
 
 int heap_insert(heap_t *heap, void *data, int priority)
 {
-  node_t *new_node;
+  node_t new_node;
+
+  // Check heap and internal array are not null before starting
+  util_check_m(heap != NULL, "Heap cannot be null, terminating...\n");
+  util_check_m(heap->nodes != NULL, "Heap nodes cannot be null, terminating...\n");
+
+  // Init new node
+  new_node.data = data;
+  new_node.priority = priority;
+
+  // Increment heap size and realloc heap if full
+  int position = heap->count++;
+  if (heap->count == heap->capacity)
+  {
+    fprintf(stdout, "Heap full, reallocating...\n");
+    heap->nodes = util_realloc(heap->nodes, 2 * (heap->capacity) * sizeof(node_t));
+    heap->capacity *= 2;
+  }
+
+  // Insert node starting from bottom and pushing it up, if necessary
+  fprintf(stdout, "Inserting new node with priority %d...\n", priority);
+  position = heap_move_up(heap, new_node, position);
+  heap_stats(heap);
+
+  return position;
+}
+
+int heap_extract(heap_t *heap, void **data)
+{
+  node_t node;
+
+  fprintf(stdout, "Extracting root node with priority %d...\n", heap->nodes[0].priority);
+  swap(heap, 0, heap->count - 1);
+  node = heap->nodes[heap->count - 1];
+
+  // Decremwnt heap size and realloc heap if halved capacity is enough
+  heap->count--;
+  if (heap->count * 2 == heap->capacity)
+  {
+    fprintf(stdout, "Heap half empty, reallocating...\n");
+    heap->nodes = util_realloc(heap->nodes, (heap->count) * sizeof(node_t));
+    heap->capacity /= 2;
+  }
+  data = node.data;
+
+  heap_heapify(heap, 0);
+  heap_stats(heap);
+
+  return 1;
+}
+
+int heap_update(heap_t *heap, int position, int newPriority, void (*freeData)(void *))
+{
+  node_t old_node;
 
   // Check heap and internal array are not null before starting
   if (heap == NULL || heap->nodes == NULL)
@@ -28,56 +87,11 @@ int heap_insert(heap_t *heap, void *data, int priority)
     return 0;
   }
 
-  // Init new node
-  new_node = (node_t *)util_malloc(sizeof(node_t));
-  new_node->data = data;
-  new_node->priority = priority;
-
-  // Increment heap size and realloc heap if full
-  int position = heap->count++;
-  if (heap->count == heap->capacity)
-  {
-    heap->nodes = util_realloc(heap->nodes, 2 * (heap->capacity));
-    heap->capacity *= 2;
-  }
-
-  // Insert node starting from bottom and pushing it up, if necessary
-  position = heap_move_up(heap, new_node, position);
-
-  return position;
-}
-
-int heap_extract(heap_t *heap, void **data)
-{
-  node_t *node;
-  
-  swap(heap, 0, heap->count - 1);
-  node = heap->nodes[heap->count - 1];
-  heap->count--;
-  data = node->data;
-  free(node);
-  
-  heapify(heap, 0);
-  
-  return 1;
-}
-
-int heap_update(heap_t *heap, int position, int newPriority, void (*freeData)(void *))
-{
-  int position;
-  node_t *old_node;
-
-  // Check heap and internal array are not null before starting
-  if (heap == NULL || heap->nodes == NULL || heap->nodes[position] == NULL)
-  {
-    return 0;
-  }
-
   // Init new node from the old one
-  old_node = heap->nodes[position]->data;
-  node_t *new_node = (node_t *)util_malloc(sizeof(node_t));
-  new_node->data = old_node;
-  new_node->priority = newPriority;
+  old_node = heap->nodes[position];
+  node_t new_node;
+  new_node.data = old_node.data;
+  new_node.priority = newPriority;
 
   if (heap_compare(old_node, new_node, heap->type) < 0)
   {
@@ -91,15 +105,16 @@ int heap_update(heap_t *heap, int position, int newPriority, void (*freeData)(vo
   // Free old node
   if (freeData != NULL)
   {
-    freeData(old_node->data);
+    freeData(old_node.data);
   }
-  free(old_node);
 
   return position;
 }
 
 void heap_destroy(heap_t *heap, void (*freeData)(void *))
 {
+  fprintf(stdout, "Freeing heap...\n");
+
   if (heap == NULL)
   {
     return;
@@ -110,10 +125,7 @@ void heap_destroy(heap_t *heap, void (*freeData)(void *))
     for (int i = 0; i < heap->count; ++i)
     {
       if (freeData != NULL)
-      {
-        freeData(heap->nodes[i]->data);
-      }
-      free(heap->nodes[i]);
+        freeData(heap->nodes[i].data);
     }
 
     free(heap->nodes);
@@ -122,25 +134,20 @@ void heap_destroy(heap_t *heap, void (*freeData)(void *))
   free(heap);
 }
 
-int heap_compare(node_t *a, node_t *b, int type)
+// Private Methods
+
+int heap_compare(node_t a, node_t b, int type)
 {
   int result = 0;
-
-  // Check nodes are not null before starting
-  if (a == NULL || b == NULL)
-  {
-    fprintf(stderr, "Cannot compare null nodes!");
-    exit(EXIT_FAILURE);
-  }
 
   // Compare priorities depending on heap type, if MIN_HEAP, standard compare, otherwise inverted compare
   if (type == MIN_HEAP)
   {
-    result = a->priority - b->priority;
+    result = a.priority - b.priority;
   }
   else
   {
-    result = b->priority - a->priority;
+    result = b.priority - a.priority;
   }
 
   return result;
@@ -151,30 +158,32 @@ int heap_heapify(heap_t *heap, int position)
   int l, r, first;
   l = LEFT(position);
   r = RIGHT(position);
-  if (l < heap->count && heap_compare(heap->nodes[l], heap->nodes[position], heap->type) > 0)
+  if (l < heap->count && heap_compare(heap->nodes[l], heap->nodes[position], heap->type) < 0)
     first = l;
   else
     first = position;
-  if (r < heap->count && heap_compare(heap->nodes[r], heap->nodes[first], heap->type) > 0)
+  if (r < heap->count && heap_compare(heap->nodes[r], heap->nodes[first], heap->type) < 0)
     first = r;
   if (first != position)
   {
     swap(heap, position, first);
-    heapify(heap, first);
+    heap_heapify(heap, first);
   }
-  return;
+  return 1;
 }
 
-int heap_move_down(heap_t *heap, node_t *node, int position)
+int heap_move_down(heap_t *heap, node_t node, int position)
 {
   // Insert node at the current position
   heap->nodes[position] = node;
 
   // Rebuild heap
   heap_heapify(heap, position);
+
+  return 1;
 }
 
-int heap_move_up(heap_t *heap, node_t *node, int position)
+int heap_move_up(heap_t *heap, node_t node, int position)
 {
   // Move nodes down the heap
   while (position >= 1 && heap_compare(heap->nodes[PARENT(position)], node, heap->type) > 0)
@@ -197,9 +206,23 @@ int swap(heap_t *heap, int a, int b)
   }
 
   // Swap a and b
-  node_t *temp = heap->nodes[b];
+  node_t temp = heap->nodes[b];
   heap->nodes[b] = heap->nodes[a];
   heap->nodes[a] = temp;
 
   return 1;
+}
+
+void heap_stats(heap_t *heap)
+{
+  util_check_m(heap != NULL, "Heap cannot be null, terminating...\n");
+  util_check_m(heap->nodes != NULL, "Heap nodes cannot be null, terminating...\n");
+  fprintf(stdout, "Heap Stats:\ttype = %s\tcount = %d\tcapacity = %d\tarray: ", heap->type == 0 ? "MIN_HEAP" : "MAX_HEAP", heap->count, heap->capacity);
+
+  for (int i = 0; i < heap->count; ++i)
+  {
+    fprintf(stdout, "%d ", heap->nodes[i].priority);
+  }
+
+  fprintf(stdout, "\n");
 }
--- heap/heapPrivate.h
+++ heap/heapPrivate.h
@@ -12,17 +12,18 @@ typedef struct node {
 } node_t;
 
 struct heap {
-  node_t **nodes;
+  node_t *nodes;
   int count;
   int capacity;
   int type;
 };
 
 /* Private methods */
-extern int heap_compare(node_t *a, node_t *b, int type);
+extern int heap_compare(node_t a, node_t b, int type);
 extern int heap_heapify(heap_t *heap, int position);
-extern int heap_move_down(heap_t *heap, node_t *node, int position);
-extern int heap_move_up(heap_t *heap, node_t *node, int position);
+extern int heap_move_down(heap_t *heap, node_t node, int position);
+extern int heap_move_up(heap_t *heap, node_t node, int position);
 extern int swap(heap_t *heap, int a, int b);
+extern void heap_stats(heap_t *heap);
 
 #endif
